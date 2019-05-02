b1fe9a72a52bfb225fb2a22fb8cbb63ee6007580
Hyegyeong310 <hyegyeong310@gmail.com>
2019-05-02 15:36:21 +0900

    create TodoInputContainer, TodoListContainer

--- src/containers/TodoInputContainer.js
+++ src/containers/TodoInputContainer.js
@@ -8,8 +8,38 @@ import * as inputActions from '../modules/index';
 import * as todosActions from '../modules/todos';
 
 class TodoInputContainer extends Component {
+  id = 1;
+  getId = () => {
+    return ++this.id;
+  };
+
+  handleChange = e => {
+    const { value } = e.target;
+    const { InputActions } = this.props;
+    InputActions.setInput(value);
+  };
+
+  handleInsert = () => {
+    const { InputActions, TodosActions, value } = this.props;
+    const todo = {
+      id: this.getId(),
+      text: value,
+      done: false
+    };
+    TodosActions.insert(todo);
+    InputActions.setInput('');
+  };
   render() {
-    return <TodoInput />;
+    const { value } = this.props;
+    const { handleChange, handleInsert } = this;
+
+    return (
+      <TodoInput
+        onChange={handleChange}
+        onInsert={handleInsert}
+        value={value}
+      />
+    );
   }
 }
 
